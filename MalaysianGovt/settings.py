c2f3f772a3c7aba57f655e5e9a730bb42ee6fd89
irene poon <aiphin@hotmail.com>
2024-10-16 23:30:42 +0530

    Update settings.py

--- MalaysianGovt/settings.py
+++ MalaysianGovt/settings.py
@@ -84,7 +84,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Laws', 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
